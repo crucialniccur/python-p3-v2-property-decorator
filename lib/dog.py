be3b37ac4c0483394780957349d240a3f5b45f8b
Mati Joseph <crucialniccur@gmail.com>
2025-05-12 18:46:54 +0300

    rewrite the function

--- lib/dog.py
+++ lib/dog.py
@@ -15,14 +15,20 @@ class Dog:
         self.name = name
         self.breed = breed
 
-    def set_name(self, name):
+    @property
+    def name(self):
+        """The name property"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name
+
         else:
             raise ValueError(
                 "Name must be string between 1 and 25 characters.")
-
-    name = property(get_name, set_name)
 
     def get_breed(self):
         return self._breed
